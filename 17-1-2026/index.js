78d945389407212451c9cab5f45ea001a7eb0183
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2026-01-19 08:51:25 +0530

    operators

---
+++ 17-1-2026/index.js
@@ -0,0 +1,45 @@
+//operators
+
+//arithmetic
+// + - * / %
+
+// let a =22
+// let b = 4
+// console.log(a%b)
+
+//comparison
+// == === != !== > < >= <=
+// let a =21
+// let b = 22
+
+// console.log(a <= b)
+
+//logical
+//&& || !
+
+// let a =21
+// let b = 22
+
+//&& if one condition is false then return is false
+// console.log(true && true && true && true && true && false);
+
+//|| if one condition is ture then return is ture
+// console.log(false ||false);
+
+//!
+// let a = 10
+// console.log(!!a)
+
+//assignment
+// = += -= *= /= %=
+//
+//bitwise
+const a = 9; // 01010
+const b = 2; // 32 bit = 00000000 0000000 00000000 00000010
+
+//30
+
+// value > binary > binary > value
+// console.log(~2); // 32 bit = 11111111 11111111 11111111 11111101
+// console.log((a).toString(2)); //1011
+
