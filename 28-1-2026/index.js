596b3879ec32c17d2c6cea3c93b45846b2cec8d0
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2026-01-28 09:41:40 +0530

    array, index, push/pop, length, access/update element, shift/unshift

---
+++ 28-1-2026/index.js
@@ -0,0 +1,25 @@
+//array
+//           0   1    2   3   4  5 .... n
+// const arr = [10, 20, 30];
+
+// arr[2] = "str"
+
+//.push()
+// let b = arr.push(40)
+
+//.pop()
+// let a = arr.pop()
+// console.log(b)
+const arr = [10, 20, 30];
+// let b = arr.push(40)
+
+// .length
+
+// console.log(arr[2])
+// console.log(arr[arr.length-1])
+// console.log()
+
+//shift and unshift
+
+// arr.unshift(100)
+// console.log(arr)
